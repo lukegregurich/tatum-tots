6808616c19e4e88943a19e104f982c94a8383085
Luke Gregurich <lgreguri@purdue.edu>
2018-05-17 11:18:56 -0400

    create li element, use new method definitions

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const app = {
-    init: function(formSelector) {
+    init(selectors) { // same as init: function(selectors)
         this.max = 0;
+        this.list = document.querySelector(selectors.listSelector)
+        
         document
-            .querySelector(formSelector)
+            .querySelector(selectors.formSelector)
             .addEventListener('submit', (ev) => {
                 ev.preventDefault()
                 this.handleSubmit(ev)
             })
     },
 
-    handleSubmit: function(ev){
+    renderListItem(flick){
+        const item = document.createElement('li')
+        item.textContent = flick.name
+        return item
+    },
+
+    handleSubmit(ev){
         const f = ev.target
         const flick = {
             id: ++this.max, 
             name: f.flickName.value,
         }
-        console.log(flick)
+        const item = this.renderListItem(flick)
+        this.list.appendChild(item)
         f.reset()
     },
-
-
 }
 
-app.init('#flickForm')
+app.init({
+    formSelector: '#flickForm',
+    listSelector: '#flickList',
+})
